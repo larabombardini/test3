bd9e5ebf1828f2b2abc4fd724d8c989d5a6e9bbd
trieuris3 <trieuris3@lab.local>
2018-06-19 14:39:47 +0200

    initial

---
+++ src/main/java/it/enaip/corsojava/app/App.java
@@ -0,0 +1,13 @@
+package it.enaip.corsojava.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
